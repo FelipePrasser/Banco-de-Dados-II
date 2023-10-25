f31caf00a89175e401640d86675c4f7dff527c3a
FelipePrasser <92392630+FelipePrasser@users.noreply.github.com>
2023-10-24 21:43:47 -0300

    Add files via upload

---
+++ AV3-c/Carro_202310242140.sql
@@ -0,0 +1,6 @@
+INSERT INTO av3c.Carro (marca,modelo,ano_fabricacao,ano_modelo,litragem,cambio,km,combustivel,cor) VALUES
+	 ('Volkswagen','Golf','2012-01-01','2012-01-01',1.6,'Manual',60000,'Flex','Branco'),
+	 ('Ford','Mustang','1969-01-01','1969-01-01',5.8,'Manual',120000,'Gasolina','Azul'),
+	 ('Bugatti','Chiron','2016-01-01','2016-01-01',8.0,'Automático',10000,'Gasolina','Preto/Azul'),
+	 ('Chevrolet','Opala Comodoro','1979-01-01','1979-01-01',4.0,'Manual',184000,'Gasolina','Vermelho'),
+	 ('Honda','Civic SI','2010-01-01','2011-01-01',1.8,'Manual',115000,'Gasolina','Cinza');
